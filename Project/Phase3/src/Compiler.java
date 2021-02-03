7eacd19beb87f3a27212f15a48da52dc123dbc76
alibalapour <alibalapour93.ab@gmail.com>
2021-02-03 23:07:49 +0330

    Organized Branch created and this is First commit for this branch.

---
+++ Project/Phase3/src/Compiler.java
@@ -0,0 +1,19 @@
+import java.io.FileReader;
+import java.io.IOException;
+import MyScanner.MyScanner;
+
+
+public class Compiler{
+    private static Compiler instance = new Compiler();
+    public static Compiler getInstance() {
+        return instance;
+    }
+
+    public void compile(String inputFileName, String OutputFileName) throws IOException {
+        // parsing phase call
+        FileReader fileReader = new FileReader(inputFileName);
+        MyScanner scanner = new MyScanner(fileReader);
+        
+        // codeGen phase call
+    }
+}
