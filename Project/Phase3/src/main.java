81e09b1cbbd9c412d9caf20368320017c9f20b84
alibalapour <alibalapour93.ab@gmail.com>
2021-02-09 16:08:42 +0330

    cgenPrint added but is not complete.

--- Project/Phase3/src/main.java
+++ Project/Phase3/src/main.java
@@ -3192,9 +3192,31 @@ class CodeGen {
     }
 
 
-
-
     private void cgenPrint(Node node) {
+        // get node's description
+        addToText("# Print something");
+        if(node.getNodeValueType().equals("INT")){
+            addToText("li $v0, 1");
+            // get variable name and use addToText("lw $a0, 'variable name'")
+            // check if variable is array, then use addToText("lw $a0, 0($a0)")
+            addToText("syscall");
+        }
+
+        //else if(node.getNodeValueType().equals("BOOL")){}
+
+        else if(node.getNodeValueType().equals("DOUBLE")){
+            addToText("li $v0, 2");
+            // get variable name and use addToText("lw $a0, 'variable name'")
+            // check if variable is array, then use addToText("lw $a0, 0($a0)")
+            addToText("mtc1 $a0, $f12");  // http://ww2.cs.fsu.edu/~dennis/teaching/2013_summer_cda3100/week5/week5-day2.pdf
+            addToText("syscall");
+        }
+
+        else if(node.getNodeValueType().equals("STRING")){
+            addToText("li $v0, 4");
+            //addToText("la $a0, 'Varibale Name'");
+            addToText("syscall");
+        }
 
     }
 
